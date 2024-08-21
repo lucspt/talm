4c70dce0c307fc7dfc4fa5809f6192bf1e430a38
lucspt <149544385+lucspt@users.noreply.github.com>
2024-08-21 08:42:49 -0700

    refactor(scripts,config): move tokenizer trainer class to script file

--- src/project_llm/config/tokenizer.py
+++ src/project_llm/config/tokenizer.py
@@ -6,7 +6,6 @@ from .root import RootConfig
 
 @dataclass
 class TokenizerConfig:
-    vocab_size: int = 1000
     special_tokens: set[str] = field(default_factory=lambda: {"<|endoftext|>"})
-    train_text_file: Path = RootConfig.data_dir / "fineweb-tokenizer-train.txt"
-    save_path: Path = RootConfig.tokenizer_path
+    tokenizer_train_file: Path = RootConfig.data_dir / "fineweb-tokenizer-train.txt"
+    registry_dir: Path = RootConfig.tokenizer_dir
