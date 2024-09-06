a0b8ae0bf1e0ef1080125e2721fb9bc0f06e95a3
lucspt <149544385+lucspt@users.noreply.github.com>
2024-09-06 11:03:20 -0700

    feat(scripts): use mixed precision in model training if available

--- src/talm/scripts/model/trainer.py
+++ src/talm/scripts/model/trainer.py
@@ -1,9 +1,10 @@
 import time
 import inspect
-from typing import Any, Generic, Literal, TypeVar, Optional
+from typing import Any, Generic, Literal, TypeVar
 from logging import Logger
 from pathlib import Path
-from collections.abc import Iterable
+from contextlib import contextmanager
+from collections.abc import Iterable, Generator
 
 import torch
 from gressbar import ProgressBar
@@ -64,19 +65,24 @@ class BaseTrainer(Generic[DLType]):
             checkpoint_dir (PathLike): A string or `Path` pointing to a file to save model checkpoints to.
             logger (Logger): A python Logger object to perform logging.
             gradient_clip_value (float): The clip value to clip gradient's with.
+            seed (int): The seed to train with.
             logging_strategy (Literal["steps", "epochs"]): Whether to log at the end of epochs or at the end of steps.
             logging_interval (int): Logging, and therefore evaluation, will be performed at this interval.
                 This respects `logging_strategy`, so a value of `20` means to log metrics every 20 steps.
         """
 
-        self.device: str
         if torch.cuda.is_available():
-            self.device = "cuda"
+            self.device_type = "cuda"
         elif torch.backends.mps.is_built() and torch.backends.mps.is_available():
-            self.device = "mps"
+            self.device_type = "mps"
         else:
-            self.device = "cpu"
+            self.device_type = "cpu"
 
+        self._is_autocast_available: bool = torch.amp.is_autocast_available(  # type: ignore
+            device_type=self.device_type
+        )
+
+        self.device = self.device_type
         self.seed = seed
         self.model_name = model_name
         self.optimizer = optimizer
@@ -110,7 +116,6 @@ class BaseTrainer(Generic[DLType]):
 
     def maybe_init_logging_file(self) -> None:
         if not self.log_file.exists() or is_file_empty(self.log_file):
-            self.log_file.touch(exist_ok=True)
             self._log_msg(" ".join(self.metrics_to_log), mode="w")
 
     def log_metrics(self, **metrics: float | str | int) -> None:
@@ -130,6 +135,23 @@ class BaseTrainer(Generic[DLType]):
         f = self.checkpoint_dir / f"epoch_{epoch}.pt"
         self.logger.info(f"Saving model checkpoint to {f}")
         torch.save(ckpt, f)
+
+    @contextmanager
+    def compute_loss_context_manager(self) -> Generator[None, None, None]:
+        if self._is_autocast_available:
+            with torch.autocast(device_type=self.device_type, dtype=torch.bfloat16):
+                yield None
+        else:
+            yield None
+
+    def compute_loss(
+        self, x: torch.Tensor, y: torch.Tensor, model: Model
+    ) -> torch.Tensor:
+        """Do a forward pass and compute the loss"""
+        with self.compute_loss_context_manager():
+            logits = self.model(x)
+            loss = model.compute_loss(logits, y)
+        return loss
 
     def iter_dataloader(
         self, dataloader: DLType
@@ -152,9 +174,7 @@ class BaseTrainer(Generic[DLType]):
         mean_loss = 0.0
         for i, (x, y) in enumerate(self.iter_dataloader(self.train_dataloader)):
             x, y = x.to(device), y.to(device)
-            logits = model(x)
-            loss = model.compute_loss(logits, y)
-
+            loss = self.compute_loss(x, y, model=model)
             optimizer.zero_grad(set_to_none=True)
             loss.backward()  # type: ignore
             grad_norm = torch.nn.utils.clip_grad_norm_(
@@ -166,13 +186,15 @@ class BaseTrainer(Generic[DLType]):
             mean_loss += lossi
             self.current_step += 1
             self.on_train_step_end(step=i + 1, train_loss=lossi)
-        mean_loss /= i + 1
+        mean_loss /= i
         return (mean_loss, lr, grad_norm)
 
     def val_loop(
-        self, model: Model, device: str, n_steps: Optional[int] = None
+        self,
+        model: Model,
+        device: str,
     ) -> float:
-        """Perform a validation over `val_dataloader`. Specify
+        """Perform a validation over `val_dataloader`.
 
         Returns:
         `float`: The mean loss over all validation of `val_dataloader`.
@@ -182,12 +204,9 @@ class BaseTrainer(Generic[DLType]):
         with torch.inference_mode():
             for i, (x, y) in enumerate(self.iter_dataloader(self.val_dataloader)):
                 x, y = x.to(device), y.to(device)
-                logits = model(x)
-                loss = model.compute_loss(logits, y)
+                loss = self.compute_loss(x, y, model=model)
                 mean_loss += loss.item()
-                if i + 1 == n_steps:
-                    break
-        mean_loss /= i + 1
+        mean_loss /= i
         return mean_loss
 
     def on_epoch_start(self, epoch: int) -> None:
@@ -196,8 +215,10 @@ class BaseTrainer(Generic[DLType]):
         )
 
     def on_train_step_end(self, step: int, train_loss: float) -> None:
+        info = {"train loss": f"{train_loss:.4f}"}
         if self.log_strategy == "steps" and step % self.log_interval == 0:
-            val_loss = self.val_loop(model=self.model, device=self.device, n_steps=1)
+            val_loss = self.val_loop(model=self.model, device=self.device)
+            info["val loss"] = f"{val_loss:.4f}"
             self.log_metrics(
                 step=step,
                 train_loss=train_loss,
@@ -205,7 +226,7 @@ class BaseTrainer(Generic[DLType]):
             )
         self.progress_bar.update(
             step,
-            info={"train loss": f"{train_loss:.4f}"},
+            info=info,
             finished=False,
         )
 
@@ -235,15 +256,20 @@ class BaseTrainer(Generic[DLType]):
         if self.log_strategy == "epochs":
             self.log_metrics(epoch=epoch, train_loss=train_loss, val_loss=val_loss)
 
+    def _set_default_dtype(self) -> None:
+        if self.device_type == "cuda" and torch.cuda.is_bf16_supported():
+            torch.set_default_dtype(torch.bfloat16)  # type: ignore
+
     def on_train_begin(self) -> None:
         self.maybe_init_logging_file()
         torch.set_float32_matmul_precision("high")
+        self._set_default_dtype()
 
     def train(self) -> None:
         """Train `self.model` over `self.epochs` epochs."""
         self.on_train_begin()
-        for epoch in range(self.epochs):
-            self.on_epoch_start(epoch=epoch + 1)
+        for epoch in range(1, self.epochs + 1):
+            self.on_epoch_start(epoch=epoch)
             t_start = time.time()
             train_loss, lr, grad_norm = self.train_loop(
                 model=self.model,
@@ -254,7 +280,7 @@ class BaseTrainer(Generic[DLType]):
             val_loss = self.val_loop(model=self.model, device=self.device)
             t = time.time() - t_start
             self.on_epoch_end(
-                epoch + 1,
+                epoch,
                 train_loss=train_loss,
                 val_loss=val_loss,
                 lr=lr,
